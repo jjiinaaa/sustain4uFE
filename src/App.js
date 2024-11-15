d076de811ba504352fcb65e7367b8caefa291614
jjiinaaa <qka0103@gmail.com>
2024-11-16 02:34:35 +0900

    fix: Signup 파일명 변경

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Start from "./pages/Start";
-import SignUp from "./pages/SignUp";
+import SignUp from "./pages/Signup";
 
 function App() {
   return (
