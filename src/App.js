d9ac5b0edbfe560c1534765bfc02d5ba6b48877b
minsong <minsong05@inha.edu>
2024-11-17 15:16:11 +0900

    main페이지 개발

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Start from "./pages/Start";
 import SignUp from "./pages/SignUp";
+import Main from "./pages/Main";
+
 
 function App() {
   return (
@@ -8,6 +10,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Start />} />
         <Route path='/signup' element={<SignUp />} />
+        <Route path='/main' element={<Main />} />
       </Routes>
     </Router>
   );
