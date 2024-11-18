0c9b84ddb2db7c88b1e9ccfc16ad4e64ce5ecd18
jjiinaaa <qka0103@gmail.com>
2024-11-18 13:34:04 +0900

    style: 메인페이지 반응형 및 근로최대가능시간 연결

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,69 +1,34 @@
-import React from 'react';
-import styled from 'styled-components';
-import Arrow from '../assets/image/arrow.svg';
-
-export default Main;
-
-// Styled Components
-const Tobbar = styled.div`
-  width: 100%;
-  position: relative;
-  font-family: Pretendard;
-  font-weight: 900;
-  color: #5c5b5b;
-  text-align: center;
-  @media screen and (max-width: 767px) {
-    padding: 1vh 0;
-    font-size: 1rem;
-  }
-  @media screen and (min-width: 768px) {
-    padding: 2vh 0;
-    font-size: 1.5rem;
-  }
-`;
-
-const ArrowIconBox = styled.div`
-  cursor: pointer;
-  position: absolute;
-  left: 20px;
-  top: 60%;
-  width: 13px;
-  height: 22px;
-  transform: translateY(-50%);
-`;
-
-const ArrowIcon = styled.img`
-  width: 100%;
-  height: 100%;
-`;
+import React from "react";
+import styled from "styled-components";
+import { Tobbar } from "../components/Tobbar";
+import Arrow from "../assets/image/arrow.svg";
+import { Link } from "react-router-dom";
 
 const Container = styled.div`
-  width: 80%;
-  height: 10vh;
+  height: 82vh;
   display: flex;
-  margin-top: 10%;
   flex-direction: column;
   align-items: center;
 
   @media screen and (max-width: 767px) {
     width: 82%;
-    height: 10vh;
+    margin-top: 10%;
     padding: 0 9%;
   }
   @media screen and (min-width: 768px) {
-    width: 40%;
-    height: 10vh;
-    padding: 0 30%;
+    width: 70%;
+    margin-top: 2%;
+    padding: 0 15%;
   }
 `;
 
 const MessageContainer = styled.div`
   width: 100%;
   @media screen and (max-width: 767px) {
-    margin: 44px 0 10px;
+    margin: 0 0 6%;
   }
   @media screen and (min-width: 768px) {
-    margin: 40px 0 5px;
+    margin: 0 0 3%;
   }
 `;
 
@@ -71,15 +36,15 @@ const WelcomeText = styled.h1`
   font-family: Pretendard;
   font-weight: 900;
   color: #5c5b5b;
-  margin: 0;
-  padding: 0 3%;
   @media screen and (max-width: 767px) {
     font-size: 1rem;
     margin: 0 0 10px;
+    padding-left: 6%;
   }
   @media screen and (min-width: 768px) {
-    font-size: 1.7rem;
+    font-size: 1.5rem;
     margin: 0 0 15px;
+    padding-left: 8%;
   }
 `;
 
@@ -88,47 +53,52 @@ const Subtitle = styled.p`
   font-weight: 300;
   color: #5c5b5b;
   margin: 0;
-  padding: 0 3%;
   @media screen and (max-width: 767px) {
     font-size: 0.65rem;
+    padding-left: 6%;
   }
   @media screen and (min-width: 768px) {
-    font-size: 0.9rem;
-    margin: 0 0 10px;
+    font-size: 0.8rem;
+    padding-left: 8%;
   }
 `;
 
-const Card = styled.div`
+const Card = styled(Link)`
   width: 80%;
-  max-width: 400px;
   background-color: #ffffff;
   padding: 30px;
   border-radius: 10px;
-  margin-bottom: 16px;
   display: flex;
   flex-direction: column;
   cursor: pointer;
-  box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.1);
+  box-shadow: 0px 0px 8px rgba(0, 0, 0, 0.1);
   text-align: left;
   transition: background-color 0.3s ease;
   position: relative;
-  
+  text-decoration: none;
+
+  @media screen and (max-width: 767px) {
+    margin: 0 0 5%;
+  }
+  @media screen and (min-width: 768px) {
+    margin: 0 0 2%;
+  }
+
   &:hover {
     background-color: #005bac;
 
-    h2, p {
+    h2,
+    p {
       color: white;
     }
-}
+  }
 `;
-
 
 const CardTitle = styled.h2`
   font-size: 1rem;
   font-weight: bold;
   color: #5c5b5b;
   transition: color 0.3s ease;
-  
 `;
 
 const CardSubtitle = styled.p`
@@ -156,24 +126,18 @@ const ArrowIconTurn = styled.img`
 
 const Footer = styled.footer`
   width: 100%;
+  height: 10vh;
+  display: fixed;
+  bottom: 0;
   background-color: #ffffff;
   color: #5c5b5b;
-  padding: 40px 0;
-  position: fixed;
-  bottom: 0;
   box-shadow: 0px 4px 16px rgba(0, 0, 0, 0.2);
 `;
 
-
 function Main() {
   return (
-    <div className='App'>
-      <Tobbar>
-        <ArrowIconBox>
-          <ArrowIcon src={Arrow} alt='back' />
-        </ArrowIconBox>
-        MAIN
-      </Tobbar>
+    <>
+      <Tobbar content='MAIN' />
       <Container>
         <MessageContainer>
           <WelcomeText>Welcome, USER3124</WelcomeText>
@@ -183,29 +147,33 @@ function Main() {
           </Subtitle>
         </MessageContainer>
 
-        <Card onClick={() => console.log('Navigate to Check permitted working hours')}>
+        <Card to='/checklist1'>
           <ArrowIconTurnBox>
             <ArrowIconTurn src={Arrow} alt='go' />
           </ArrowIconTurnBox>
           <CardTitle>
-            Check permitted<br />
+            Check permitted
+            <br />
             Working hours
           </CardTitle>
           <CardSubtitle>근로 허용 시간 확인</CardSubtitle>
         </Card>
 
-        <Card onClick={() => console.log('Navigate to Unpermitted Work Categories')}>
+        <Card
+          onClick={() => console.log("Navigate to Unpermitted Work Categories")}
+        >
           <ArrowIconTurnBox>
             <ArrowIconTurn src={Arrow} alt='go' />
           </ArrowIconTurnBox>
           <CardTitle>
-            Unpermitted<br />
+            Unpermitted
+            <br />
             Work Categories
           </CardTitle>
           <CardSubtitle>국제학생 시간제 취업 불허 직종</CardSubtitle>
         </Card>
 
-        <Card onClick={() => console.log('Navigate to Employment Contract')}>
+        <Card onClick={() => console.log("Navigate to Employment Contract")}>
           <ArrowIconTurnBox>
             <ArrowIconTurn src={Arrow} alt='go' />
           </ArrowIconTurnBox>
@@ -213,9 +181,9 @@ function Main() {
           <CardSubtitle>근로계약서 출력</CardSubtitle>
         </Card>
       </Container>
-      <Footer>
-      </Footer>
-    </div>
+      <Footer />
+    </>
   );
 }
-  
+
+export default Main;
